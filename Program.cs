2c808fa60b08d3cc9181cd66be5ff8346ca0a80c
Albert Sevy <albertstorres@gmail.com>
2024-01-23 22:46:28 -0300

    Finalizando o repositório da aula Strings/MetodosAdicionais

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace MetodosAdicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Texto de teste";
+            Console.WriteLine(texto.ToLower());//IGUAL A JS. CONVERTE TUDO PARA MINÚSCOLO.
+            Console.WriteLine(texto.ToUpper());//IGUAL A JS. COVERTE TUDO PARA MAIÚSCULO.
+            Console.WriteLine(texto.Insert(6, "AQUI "));//VAI INSERIR A PARTIR DO ÍNDICE 6 A STRING PASSADA.
+            Console.WriteLine(texto.Remove(6, 4));//REMOVE 4 CARACTERES A PARTIR DO ÍNDICE 6. MAS TÁ REMOVENDO MAIS. INCLUSIVE NA AULA.
+            Console.WriteLine(texto.Length);//RETORNA 14. IGUAL AO JS
+        }
+    }
+}
